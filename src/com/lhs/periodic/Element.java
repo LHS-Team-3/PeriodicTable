53dc2b3c60639d05519fb45bb009f24e550f5828
RPerumalla9 <rohithperumalla@gmail.com>
2015-12-13 12:30:07 -0600

    Additions for Elements fields
    Added a group int and period int

--- src/com/lhs/periodic/Element.java
+++ src/com/lhs/periodic/Element.java
@@ -2,7 +2,7 @@ package com.lhs.periodic;
 
 public enum Element {
 	
-	HYDROGEN(1,1,1,1,Type.NONMETAL,"Hydrogen","H",1.01,2.2,53,1312);
+	HYDROGEN(1,1,1,1,Type.NONMETAL,"Hydrogen","H",1.01,2.2,53,1312,1,1);
 	
 	public int number;
 	public int protons;
@@ -16,8 +16,10 @@ public enum Element {
 	public double atomicRadius;
 	public int valenceElectrons;
 	public double ionizationEnergy;
+	public int group;
+	public int period;
 	
-	Element(int number, int protons, int electrons, int energyLevels, Type type, String name, String symbol, double atomicMass, double electronegativity, double atomicRadius, double ionizationEnergy) {
+	Element(int number, int protons, int electrons, int energyLevels, Type type, String name, String symbol, double atomicMass, double electronegativity, double atomicRadius, double ionizationEnergy, int group, int period) {
 		this.number = number;
 		this.protons = protons;
 		this.electrons = electrons;
@@ -34,6 +36,10 @@ public enum Element {
 		while (number>8) number-=8;
 		this.valenceElectrons = valenceElectrons;
 		this.ionizationEnergy = ionizationEnergy;
+		this.group = group;
+		this.period = period;
 	}
+
+
 	
 }
