2b916660bb998c51a8a1f8ebe128e507953db73a
Ben <googleben@sbcglobal.net>
2015-12-14 15:37:16 -0600

    JSON-ify element creation

--- src/com/lhs/periodic/Element.java
+++ src/com/lhs/periodic/Element.java
@@ -1,8 +1,13 @@
 package com.lhs.periodic;
 
+import org.json.*;
+
 public enum Element {
 	
-	HYDROGEN(1,1,1,1,Type.NONMETAL,"Hydrogen","H",1.01,2.2,53,1312,1,1);
+	//HYDROGEN((Integer)((JSONObject)Utils.masterJson.get("Hydrogen")).get("number"),1,1,1,Type.NONMETAL,"Hydrogen","H",1.01,2.2,53,1312,1,1);
+	
+	Hydrogen("Hydrogen"),
+	Helium("Helium");
 	
 	public int number;
 	public int protons;
@@ -19,25 +24,26 @@ public enum Element {
 	public int group;
 	public int period;
 	
-	Element(int number, int protons, int electrons, int energyLevels, Type type, String name, String symbol, double atomicMass, double electronegativity, double atomicRadius, double ionizationEnergy, int group, int period) {
-		this.number = number;
-		this.protons = protons;
-		this.electrons = electrons;
-		this.energyLevels = energyLevels;
-		this.type = type;
+	Element(String name) {
+		JSONObject json = Utils.masterJson.getJSONObject(name);
+		this.number = json.getInt("number");
+		this.protons = json.getInt("protons");
+		this.electrons = json.getInt("electrons");
+		this.energyLevels = json.getInt("energyLevels");
+		this.type = json.getEnum(Type.class, "type");
 		this.name = name;
-		this.symbol = symbol;
-		this.atomicMass = atomicMass;
-		this.electronegativity = electronegativity;
-		this.atomicRadius = atomicRadius;
+		this.symbol = json.getString("symbol");
+		this.atomicMass = json.getDouble("atomicMass");
+		this.electronegativity = json.getDouble("electronegativity");
+		this.atomicRadius = json.getDouble("atomicRadius");
 		//find valence electrons
 		int valenceElectrons = number;
 		if (number>2) number-=2;
 		while (number>8) number-=8;
 		this.valenceElectrons = valenceElectrons;
-		this.ionizationEnergy = ionizationEnergy;
-		this.group = group;
-		this.period = period;
+		this.ionizationEnergy = json.getDouble("ionizationEnergy");
+		this.group = json.getInt("group");
+		this.period = json.getInt("period");
 	}
 
 
--- src/com/lhs/periodic/Utils.java
+++ src/com/lhs/periodic/Utils.java
@@ -1,7 +1,20 @@
 package com.lhs.periodic;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 public class Utils {
 	
+	public static void main(String... args) {
+		System.out.println(Element.Helium.number);
+	}
+	
+	public static JSONObject masterJson = new JSONObject(Utils.readFile("elements.json"));
 	
 	/**
 	 * Calculates the number of valence electrons for a given number of electrons.
@@ -14,6 +27,16 @@ public class Utils {
 		if (electrons>2) electrons-=2;
 		while (electrons>8) electrons-=8;
 		return valenceElectrons;
+	}
+	
+	static String readFile(String path) {
+		try {
+			Charset encoding = Charset.defaultCharset();
+			byte[] encoded = Files.readAllBytes(Paths.get(path));
+			return new String(encoded, encoding);
+		} catch(Exception e) {
+			return null;
+		}
 	}
 	
 }
